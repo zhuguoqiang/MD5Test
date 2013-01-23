a29384fe0f3eb9abd7d48bc919ad57052fcb3d10
zhuguoqiang <zhuguoqiang_2010@163.com>
2013-01-23 14:26:36 +0800

    MD5加密错误，已修改
    CC_MD5(cStr, strlen(cStr), result);

--- MD5Test/DHViewController.m
+++ MD5Test/DHViewController.m
@@ -41,19 +41,37 @@
 
 - (void)encryptBtn
 {
+    
+    NSString *md5pass = [self md5:pass];
+    NSLog(@"%@",md5pass);
+    
     const char *cStr = [pass UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
     unsigned char resultt[CC_SHA1_DIGEST_LENGTH];
     
     //MD5加密
-    CC_MD5(cStr, StrLength(cStr), result);
+//    CC_MD5(cStr, StrLength(cStr), result);//    StrLength  strlen     区别？？？？？
+    CC_MD5(cStr, strlen(cStr), result);
 
-    self.MD5pass.text = [[NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-             result[0], result[1], result[2], result[3],
-             result[4], result[5], result[6], result[7],
-             result[8], result[9], result[10], result[11],
-             result[12], result[13], result[14], result[15]
-             ] lowercaseString];
+    NSString *md5str = [NSString stringWithFormat:
+                        
+                        @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                        
+                        result[0], result[1], result[2], result[3],
+                        
+                        result[4], result[5], result[6], result[7],
+                        
+                        result[8], result[9], result[10], result[11],
+                        
+                        result[12], result[13], result[14], result[15]
+                        
+                        ];
+
+    self.MD5pass.text = md5str;
+    
+
+
+
    
     
     //SHA加密
@@ -64,18 +82,52 @@
              resultt[8], resultt[9], resultt[10], resultt[11],
              resultt[12], resultt[13], resultt[14], resultt[15],
              resultt[16], resultt[17], resultt[18], resultt[19]
-             ] uppercaseString];
+             ] lowercaseString];
     
     
 }
 
+- (NSString *)md5:(NSString *)str
+
+{
+    
+    const char *cStr = [str UTF8String];
+    
+    unsigned char result[16];
+    
+    CC_MD5(cStr, strlen(cStr), result); // This is the md5 call
+    
+    return [NSString stringWithFormat:
+            
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            
+            result[0], result[1], result[2], result[3],
+            
+            result[4], result[5], result[6], result[7],
+            
+            result[8], result[9], result[10], result[11],
+            
+            result[12], result[13], result[14], result[15]
+            
+            ];
+    
+}
+
 #pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
     pass = textField.text;
     
     [textField resignFirstResponder];
+    
+//    [self encryptBtn];
    
 }
 
